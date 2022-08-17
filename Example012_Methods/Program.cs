9816aafb4d3cb4bcc059268cb51cbf50c7795883
Katy <wc-katy@yandex.ru>
2022-08-17 21:04:44 +0700

    add new version of code

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -43,6 +43,7 @@ Console.WriteLine(year);
 
 //Вид 4: Что-то принимают и что-то возвращают
 
+/* через цикл while:
 string Method4(int count, string text)
 {
     int i = 1;
@@ -59,5 +60,66 @@ string Method4(int count, string text)
 // string res = Method4(10, "aca-");
 string res = Method4(text: "==/", count: 5);
 Console.WriteLine(res);
+*/
 
+/* через цикл for:
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+
+    for (int i = 1; i <= count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
+
+string res = Method4(text: "==/", count: 5);
+Console.WriteLine(res);
+*/
 //--------------------------------------------------------------------------------------------------
+
+// Цикл в цикле. На примере табицы умножения
+
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+//--------------------------------------------------------------------------------------------------
+
+/* Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить на
+большие "К", а большие "С" заменить на маленькие "с".
+*/
+
+string text = "-Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+newText = Replase(newText, 'к', 'К');
+newText = Replase(newText, 'с', 'С');
+
+Console.WriteLine(newText);
